300078536395e1021cb073cd9e29923f9e037dc8
holyhoppsan <daniel.hall@live.se>
2024-03-22 16:54:21 +0000

    Added a wider dataset for training

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Download conda
-# mkdir -p ~/miniconda3
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-# rm -rf ~/miniconda3/miniconda.sh
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
 
 # echo "Initialize shell"
-# # Initialize conda in bash
-# ~/miniconda3/bin/conda init bash
+# Initialize conda in bash
+~/miniconda3/bin/conda init bash
 
-conda init zsh
+# conda init zsh
 
 # Init environment
 conda create --name learntosearch python=3.11
